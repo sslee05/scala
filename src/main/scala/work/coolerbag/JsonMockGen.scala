5fa2695e46c5989d0f96ead2f15db8929bf3fa3c
sslee05 <sslee05@coupang.com>
2019-11-12 19:26:59 +0900

    json file gen

--- src/main/scala/work/coolerbag/JsonMockGen.scala
+++ src/main/scala/work/coolerbag/JsonMockGen.scala
@@ -6,14 +6,16 @@ import java.nio.file.{Files, Paths}
 import scala.io.Source
 import spray.json._
 import DefaultJsonProtocol._
-import work.coolerbag.data.{BagCode, Coolerbag, CoolerbagData, CustomerAddr}
+import work.coolerbag.data.{BagCode, CoolerBagCollected, CoolerBagCollectedCommand, Coolerbag, CoolerbagData, CustomerAddr}
 
 object JsonMockGen {
+
+  val bagCodeFormat = "%05d"
 
   def main(args: Array[String]): Unit = {
 
     println(args)
-    var fileName = "/Users/sslee05/work/coolerbag/test.txt"
+    var fileName = "/Users/sslee05/work/coolerbag/source.txt"
     if(!args.isEmpty)
      fileName = args(0)
 
@@ -24,10 +26,13 @@ object JsonMockGen {
     implicit val bagCodeFormat = jsonFormat2(BagCode)
     implicit val coolerbagFormat = jsonFormat4(Coolerbag)
     implicit val coolerbagDataFormat = jsonFormat1(CoolerbagData)
+    implicit val coolerBagCollectedFormat = jsonFormat4(CoolerBagCollected)
+    implicit val coolerBagCollectedCommand = jsonFormat8(CoolerBagCollectedCommand)
 
     val rs = (Source fromFile fileName).getLines().foldLeft(List.empty[CustomerAddr])((xs,s) => s.parseJson.convertTo[CustomerAddr] :: xs )
+
     val coolerBags = rs.zipWithIndex.map { it =>
-      Coolerbag(it._1.memberSrl, it._1.addr1, it._1.addr2, genBagCode(it._2))
+      Coolerbag(it._1.memberSrl, it._1.addr1, it._1.addr2, genBagCode(it._2 * 3))
     }
 
     coolerBags.foreach(println)
@@ -37,9 +42,31 @@ object JsonMockGen {
 
     Files.write(Paths.get("/Users/sslee05/work/coolerbag/gen-mockdata.json"),outputJson.getBytes(StandardCharsets.UTF_8))
 
+
+    val collectedDatas = coolerBags.map{it =>
+      CoolerBagCollectedCommand(
+        it.memberSrl.toLong,
+        -1L,
+        it.addr1,
+        it.addr2,
+        it.bagCodes.size,
+        0,
+        it.bagCodes.map(b => CoolerBagCollected(b.code, "COLLECTED", "SCAN",false)),
+        "1573527420"
+      )
+    }
+
+    val collectedJson = collectedDatas.toJson.toString()
+
+    println("###################")
+    println(collectedDatas)
+    println(collectedJson)
+
+    Files.write(Paths.get("/Users/sslee05/work/coolerbag/collected.json"),collectedJson.getBytes(StandardCharsets.UTF_8))
+
   }
 
-  def genBagCode(preIdx: Int): List[BagCode] =
-    Range(0,3).map(it => BagCode(s"C${preIdx}000${it}", "ORDERED")).toList
+  def genBagCode(start: Int): List[BagCode] =
+    Range(start,start+3).map(it => BagCode(s"C1-${bagCodeFormat.format(it)}", "ORDERED")).toList
 
 }
--- src/main/scala/work/coolerbag/package.scala
+++ src/main/scala/work/coolerbag/package.scala
@@ -6,4 +6,15 @@ package object data {
   case class Coolerbag(memberSrl: String, addr1: String, addr2: String, bagCodes: List[BagCode])
   case class CustomerAddr(memberSrl: String, addr1: String, addr2: String)
   case class CoolerbagData(coolerbags: List[Coolerbag])
+
+  case class CoolerBagCollected(bagCode: String,status:String, scanMethod: String, isExtra: Boolean)
+  case class CoolerBagCollectedCommand(
+       memberSrl: Long,
+       householdHash: Long,
+       addr1: String,
+       addr2: String,
+       totalCount: Int = 0,
+       damagedCount: Int = 0,
+       coolerBags: List[CoolerBagCollected],
+       requestAt: String)
 }
